c6a3789c1a83764e7f9fe99eed9c3c5a19dedb61
Michael Nobile <nobility33@gmail.com>
2016-08-19 13:03:56 -0400

    guess the number app

--- Guess the number app/program.py
+++ Guess the number app/program.py
@@ -1,5 +1,30 @@
+#import random
+import random
+
 print('--------------------------')
 print('  Guess the number game   ')
 print('--------------------------')
 print()
-#
+
+#the_number = random.randint(0, 100)
+the_number = random.randint(0, 100)
+guess = -1
+
+
+# debug
+#print(guess_text, type(guess_text))
+#print(guess, type(guess))
+
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+    if guess < the_number:
+        print('too low')
+    elif guess > the_number:
+        print('too high')
+    else:
+        print('you win!')
+print('done')
+
+
+
