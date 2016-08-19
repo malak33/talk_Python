e10a5710a6639417684d0f245400879e8911d0a5
Michael Nobile <nobility33@gmail.com>
2016-08-19 16:26:44 -0400

    guess the number app is done

--- Guess the number app/program.py
+++ Guess the number app/program.py
@@ -10,6 +10,7 @@ print()
 the_number = random.randint(0, 100)
 guess = -1
 
+name = input('Player what is your name? ')
 
 # debug
 #print(guess_text, type(guess_text))
@@ -19,11 +20,11 @@ while guess != the_number:
     guess_text = input('Guess a number between 0 and 100: ')
     guess = int(guess_text)
     if guess < the_number:
-        print('too low')
+        print('Sorry {1}, your guess of {0} was too LOW'.format(guess, name))
     elif guess > the_number:
-        print('too high')
+        print('Sorry {1}, your guess of {0} was too HIGH'.format(guess, name))
     else:
-        print('you win!')
+        print('Excellent work {1}, you win! The number was {0}.'.format(guess, name))
 print('done')
 
 
