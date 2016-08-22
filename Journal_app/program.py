f8b5eca9f1d97afd244ba5fe5a94c04ff64cefd7
Michael Nobile <nobility33@gmail.com>
2016-08-22 16:47:00 -0400

    Adding the journal app

--- Journal_app/program.py
+++ Journal_app/program.py
@@ -12,29 +12,37 @@ def print_header():
 
 
 def run_event_loop():
-
     print('What do you want to do with your journal?')
     cmd = None
+    journal_data = []   # list()
 
     while cmd != 'x':
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
 
         if cmd == 'l':
-            list_entries()
+            list_entries(journal_data)
         elif cmd == 'a':
-            add_entry()
+            add_entry(journal_data)
         elif cmd != 'x':
             print("Sorry we don't understand '{}'.".format(cmd))
     print('Done, goodbye.')
 
 
-def list_entries():
-    print('Listing...')
+def list_entries(data):
+    #    print('Listing...')
+    print('Your journal entries: ')
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx+1, entry))
 
 
-def add_entry():
-    print("Adding...")
+def add_entry(data):
+    #    print("Adding...")
+    text = input('Type your entry, <enter> to exit: ')
+    data.append(text)
+
+#importing and using additional python files
 
 
 
