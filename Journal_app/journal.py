f932118027cecb4013993095a89cb37111dae59c
Michael Nobile <nobility33@gmail.com>
2016-08-24 13:19:50 -0400

    Journal app is done

--- Journal_app/journal.py
+++ Journal_app/journal.py
@@ -1,11 +1,36 @@
+import os
+
 
 def load(name):
-    # todo: populate from the file if it exits.
-    return []
+    """
+    This method creates and loads a new journal.
+
+    :param name: The base name of the journal to load.
+    :return: A new journal data structure populated with the file data.
+    """
+    data = []
+    filename = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename) as finput:
+            for entry in finput.readlines():
+                data.append(entry.rstrip())
+
+    return data
 
 
 def save(name, journal_data):
-    pass
+    filename = get_full_pathname(name)
+    print('..... saving to {}'.format(filename))
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('./journals/' + name + '.jrl'))
+    return filename
 
 
 def add_entry(text, journal_data):
